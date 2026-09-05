5806e10b9a21e2641bef4a049ef5d50627caeaeb
mosahil01 <mos.sahil01@gmail.com>
2026-09-05 21:29:29 +0530

    feat: first leetcode problem

--- TypeScript/leetcode/1-twoSum.ts
+++ TypeScript/leetcode/1-twoSum.ts
@@ -1,6 +1,29 @@
-// const nums: number[] = [3, 4, 9, 6, 4]
-// const target: number = 9;
+const nums: number[] = [3, 14, 9, 5, 4]
+const target: number = 9;
 
-function twoSum(int: number[], target: number) {
-  return 0;
+// function twoSum(nums: number[], target: number): number[] {
+//     const seen: any = {}; /* map */
+//     for (let i = 0; i < nums.length; i++) { 
+//         const num = nums[i];
+//         if (target - num in seen) return [i, seen[target - num]];
+//         seen[num] = i;
+//     }    
+//     return seen;
+// };
+
+function twoSum(nums:number[], target: number): number[] {
+    const seen: any = {}; /* map */
+    for (let i = 0; i < nums.length; i++) {
+        seen[nums[i]] = i;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        const required = target - nums[i]; /* 9 - 3,14,9,5,4 */
+        if (seen[required] !== ) {
+            
+        }
+    }
 }
+
+
+console.log(twoSum(nums, target));
