f869481dccfe7f70b064935cd26a21685262d063
mosahel01 <mos.sahil01@gmail.com>
2026-09-10 18:47:34 +0530

    type hinting

--- TypeScript/type.ts
+++ TypeScript/type.ts
@@ -1,18 +1,50 @@
-function LoggerTimeout(loggerCallback: (s1: string, s2: string) => string) {
-    // do something
+// function LoggerTimeout(loggerCallback: (s1: string, s2: string) => string) {
+//     // do something
+// }
+
+// /*
+//     Function above is lame and unreadable
+//     TypeScript has "type" keyword to define types
+
+//     they are easier to read
+//     less prone to copying errors
+//     we only need to modify type declaration rather than everywhere its used
+// */
+
+// type LoggerCallback = (s1: string, s2: string) => string;
+
+// function typedLoggerTimeout(loggerCallback: LoggerCallback) {
+//   // do something
+// }
+
+// bootdev problem
+function interpolateComment(
+  id: number,
+  comment: string,
+  comments: (string | number)[],
+) {
+  const iterator = comments.findIndex((item) => item == id);
+  if (iterator !== -1) {
+    comments[iterator] = comment;
+  }
+  return comments;
 }
 
-/*
-    Function above is lame and unreadable
-    TypeScript has "type" keyword to define types
+console.log(interpolateComment(418, "Refresh token is missing", [
+  "salmon discount code",
+  418,
+  "I can't remember my email",
+  420,
+]));
 
-    they are easier to read
-    less prone to copying errors
-    we only need to modify type declaration rather than everywhere its used
-*/
 
-type LoggerCallback = (s1: string, s2: string) => string;
-
-function typedLoggerTimeout(loggerCallback: LoggerCallback) {
-  // do something
+function interpolateComments(
+  id: number,
+  comment: string,
+  comments: (string | number)[]
+) {
+  const iterator = comments.findIndex((item) => item === id)
+  if (iterator != -1) {
+    comments[iterator] = comment;
+  }
 }
